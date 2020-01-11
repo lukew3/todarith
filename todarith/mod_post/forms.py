6438c4a7ae87c62d8e0a3a671b55436e3a7f1361
lukew3 <lukew25073@gmail.com>
2020-01-11 15:59:43 -0500

    selectfield working for class

--- todarith/mod_post/forms.py
+++ todarith/mod_post/forms.py
@@ -1,13 +1,18 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from werkzeug import secure_filename
+
+
+classChoices=[('1', 'kindergarten'), ('2', 'first grade'), ('3', 'second grade')]
 
 class QuestionForm(FlaskForm):
     question = StringField('Question')
     answer = StringField('Answer')
-    classNumber = StringField('Class')
+    #classNumber = StringField('Class')
+
+    classNumber = SelectField('Class', choices = classChoices)
     section = StringField('Section')
     type = StringField('Type')
     #photo = StringField('Photo')
