81c7e9fdc3c790619868195e931e36472ca2e1a8
lukew3 <lukew25073@gmail.com>
2020-01-11 12:42:00 -0500

    first steps of newpost

--- todarith/mod_post/controller.py
+++ todarith/mod_post/controller.py
@@ -3,9 +3,15 @@ from todarith import db
 
 post = Blueprint('post', __name__)
 
-@post.route('/new')
+@app.route("/new", methods=['GET', 'POST'])
 def newPost():
-    return render_template('post/newpost.html')
+    form = EarnForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, description=form.description.data, photo=form.photo.data)
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for('explore'))
+    return render_template('post/newpost.html', form=form)
 
 @post.route('/edit')
 def edit():
