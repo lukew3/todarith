a2b486558d3e1dc3cc43fd260407904f546b3e4f
lukew3 <lukew25073@gmail.com>
2020-01-13 22:13:41 -0500

    changed 3 to 9

--- todarith/config.py
+++ todarith/config.py
@@ -5,7 +5,7 @@ class Config:
     #BASE_DIR = os.path.abspath(os.path.dirname(__file__))
     #SQLALCHEMY_DATABASE_URI = 'sqlite://' + os.path.join(BASE_DIR, 'app.db')
     #SQLALCHEMY_DATABASE_URI = ''
-    MYSQL_HOST = 'sql3.freemysqlhosting.net'
+    MYSQL_HOST = 'sql9.freemysqlhosting.net'
     MYSQL_USER = 'sql9318896'
     MYSQL_PASSWORD = 'whlSMpD33C'
     MYSQL_DB = 'sql9318896'
