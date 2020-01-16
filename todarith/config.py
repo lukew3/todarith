3847065e318c966b31e9c23816bd8b5484275db3
lukew3 <lukew25073@gmail.com>
2020-01-15 20:40:05 -0500

    getting some error messages but im taking a break for now

--- todarith/config.py
+++ todarith/config.py
@@ -5,7 +5,7 @@ class Config:
     #BASE_DIR = os.path.abspath(os.path.dirname(__file__))
     #SQLALCHEMY_DATABASE_URI = 'sqlite://' + os.path.join(BASE_DIR, 'app.db')
     #SQLALCHEMY_DATABASE_URI = ''
-    SQLALCHEMY_DATABASE_URI = 'mysql://sql9318896:whlSMpD33C@sql9.freemysqlhosting.net/mydatabase'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:8a9cbor4@localhost/todarith'
     MYSQL_DATABSE_HOST = 'localhost'
     MYSQL_DATABASE_USER = 'root'
     MYSQL_DATABASE_PASSWORD = '8a9cbor4'
