6510361e450080caeabb1da250142d1f156d888b
lukew3 <lukew25073@gmail.com>
2020-01-22 21:09:00 -0500

    connect to infinityfree mysql database

--- todarith/config.py
+++ todarith/config.py
@@ -5,12 +5,9 @@ class Config:
     #BASE_DIR = os.path.abspath(os.path.dirname(__file__))
     #SQLALCHEMY_DATABASE_URI = 'sqlite://' + os.path.join(BASE_DIR, 'app.db')
     #SQLALCHEMY_DATABASE_URI = ''
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:8a9cbor4@localhost/todarith'
-    MYSQL_DATABSE_HOST = 'localhost'
-    MYSQL_DATABASE_USER = 'root'
-    MYSQL_DATABASE_PASSWORD = '8a9cbor4'
-    MYSQL_DATABASE_DB = 'todarith'
-    DATABASE_CONNECT_OPTIONS = {}
+    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:8a9cbor4@localhost/todarith'
+    #SQLALCHEMY_DATABASE_URI = 'mysql://username:password@server/db'
+    SQLALCHEMY_DATABASE_URI = 'mysql://epiz_25094715:QT7BmdgeAnf0Op@sql207.epizy.com/epiz_25094715_todarith'
     CSRF_SESSION_KEY = "secret"
     SECRET_KEY = "secret"
     CSRF_ENABLED = True
