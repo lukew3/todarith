f42560e694324541f0a4cb7e2262930f05fc8fa1
lukew3 <lukew25073@gmail.com>
2020-01-29 19:24:27 -0500

    basic postgres connection

--- todarith/config.py
+++ todarith/config.py
@@ -3,6 +3,7 @@ from flaskext.mysql import MySQL
 
 class Config:
     DEBUG = True
+    """
     #BASE_DIR = os.path.abspath(os.path.dirname(__file__))
     #SQLALCHEMY_DATABASE_URI = 'sqlite://' + os.path.join(BASE_DIR, 'app.db')
     #SQLALCHEMY_DATABASE_URI = ''
@@ -15,6 +16,16 @@ class Config:
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://epiz_25094715:QT7BmdgeAnf0Op@sql207.epizy.com:3307/epiz_25094715_todarith'
+    """
+
+    POSTGRES = {
+        'user': 'lukew3',
+        'pw': '8a9cbor4',
+        'db': 'todarith',
+        'host': 'localhost',
+        'port': '5432',
+    }
+    SQLALCHEMY_DATABASE_URI = 'postgresql://%(user)s:\%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
     #CSRF_SESSION_KEY = "secret"
     SECRET_KEY = "secret"
     CSRF_ENABLED = True
