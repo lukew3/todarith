180a3981bdc96b9c445ebfdd4637c8c8e7582cb8
lukew3 <lukew25073@gmail.com>
2020-02-16 22:49:56 -0500

    restructuring search

--- todarith/mod_search/controller.py
+++ todarith/mod_search/controller.py
@@ -1,8 +1,8 @@
 from todarith.mod_search.forms import ProblemSearchForm
-from flask import flash, render_template, request, redirect
+from flask import flash, render_template, request, redirect, url_for
 from todarith.models import User, Problem, Topic
 from todarith.mod_search import search
-
+"""
 searchString = ""
 @search.route('/', methods=['GET', 'POST'])
 def index():
@@ -19,6 +19,22 @@ def search_results(search):
     search_string = searchString
     results = Problem.query.filter_by(question=search_string)
     return render_template('search/results.html', results=results)
-
+"""
 #form.topic.choices = [(row.id, row.topicName) for row in Topic.query.all()]
 #results = [(row.id, row.problem) for row in Problem.query.filter_by(problem=search_string)]
+
+@search.route('/', methods=['GET', 'POST'])
+def search_page():
+  form = ProblemSearchForm()
+  if request.method == 'POST':
+      query=form.searchQuery.data
+      results=Problem.query.filter_by(question=query).all()
+      print(results)
+      return render_template('search/results.html', query=query, results=results)
+      #return redirect(url_for('search.search_results', query=query))
+  return render_template('search/search.html', form=form)
+
+@search.route('/search_results/<query>')
+def search_results(query):
+  results = Problem.query.filter_by(problem=query).all()
+  return render_template('search/results.html', query=query, results=results)
