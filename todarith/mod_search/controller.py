fb73bb6602f9aca4e6553a468bc79cb73dc81f48
lukew3 <lukew25073@gmail.com>
2020-03-14 13:42:11 -0400

    Search submits but query isnt passed

--- todarith/mod_search/controller.py
+++ todarith/mod_search/controller.py
@@ -2,6 +2,7 @@ from todarith.mod_search.forms import ProblemSearchForm
 from flask import flash, render_template, request, redirect, url_for
 from todarith.models import User, Problem, Topic
 from todarith.mod_search import search
+from todarith import db
 """
 searchString = ""
 @search.route('/', methods=['GET', 'POST'])
@@ -25,14 +26,26 @@ def search_results(search):
 
 @search.route('/', methods=['GET', 'POST'])
 def search_page():
-  form = ProblemSearchForm()
-  if request.method == 'POST':
-      query=form.searchQuery.data
-      results=Problem.query.filter_by(question=query).all()
-      print(results)
-      return render_template('search/results.html', query=query, results=results)
+    form = ProblemSearchForm()
+    if request.method == 'POST':
+        if form.validate():
+            print("Validation successs")
+            searchQuery=form.searchQuery.data
+            print(searchQuery)
+            #results=Problem.filter_by(question=searchquery).all()
+            results = db.session.query(Problem).filter_by(question=searchQuery).all()
+            print(results)
+            return render_template('search/results.html', searchQuery=searchQuery, results=results)
+        else:
+            print("Validation failed")
+            searchQuery=form.searchQuery.data
+            print(searchQuery)
+            results = db.session.query(Problem).filter_by(question=searchQuery).all()
+            print(results)
+            return render_template('search/results.html', searchQuery=searchQuery, results=results)
+
       #return redirect(url_for('search.search_results', query=query))
-  return render_template('search/search.html', form=form)
+    return render_template('search/search.html', form=form)
 
 @search.route('/search_results/<query>')
 def search_results(query):
