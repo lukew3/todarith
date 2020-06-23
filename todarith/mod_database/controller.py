2044f6b1a557860fbb08f7c2cbe1e573e0f86425
lukew3 <lukew25073@gmail.com>
2020-06-22 20:45:50 -0400

    jquery and format work

--- todarith/mod_database/controller.py
+++ todarith/mod_database/controller.py
@@ -1,11 +1,10 @@
-from flask import (
-    current_app, request, redirect, url_for, render_template, flash, abort,
-)
+from flask import request, render_template, redirect, url_for, jsonify
 #from flask import Blueprint, request, render_template
 from todarith import db
 #from todarith.mod_auth.forms import LoginForm
 from todarith.models import Problem, Topic, User
 from todarith.mod_database.forms import AskForm
+from flask_login import current_user, login_required
 from todarith.mod_database import moddb
 
 
@@ -14,40 +13,26 @@ from todarith.mod_database import moddb
 def browse():
     return(render_template("database/browse.html"))
 
-@moddb.route('/ask')
+@moddb.route('/ask', methods=['GET', 'POST'])
 def ask():
-    form = AskForm()
-    if form.validate_on_submit():
-        redirect(url_for('database/'))
-    return render_template("database/ask.html", form=form)
+    return render_template('database/ask.html')
 
-"""
 
-@post.route('/_get_problem_input')
+@moddb.route('/_get_problem_input')
 def get_problem_input():
+    #print("Somethings")
     prob = request.args.get('problem', "", type=str)
     ans = request.args.get('answer', "", type=str)
+    cat = request.args.get('category', "", type=str)
     print("Problem:" + prob)
     print("Answer:" + ans)
+    print("Category:" + cat)
 
-    solved = checkSolved(ans)
+    #solved = checkSolved(ans)
     if current_user.is_authenticated:
         poster = current_user.id
     else:
         poster = 1
     #form.topic.choices = [(row.id, row.topicName) for row in Topic.query.all()]
-    if checkOriginal(prob, ans):
-        Problem.create(
-            question=prob,
-            answer=ans,
-            topic_id=1,
-            poster_id=poster,
-            confirmedCorrect=None,
-            difficultyLevel=None,
-            expectedTime=None,
-            hasSolution=solved
-        )
-        return jsonify(result="Problem \"" + prob + "=" + ans + "\" added")
-    else:
-        return jsonify(result="Problem \"" + prob + "=" + ans + "\" was not added")
-"""
+
+    return jsonify(result="Problem \"" + prob + "=" + ans + "\" was not added")
