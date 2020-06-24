89208ffb4ae6815a0049636c009b2a2bd7d6662e
lukew3 <lukew25073@gmail.com>
2020-06-23 23:26:21 -0400

    first problem shows on load and other problems are added to list

--- todarith/mod_learn/controller.py
+++ todarith/mod_learn/controller.py
@@ -10,6 +10,22 @@ from random import random
 def learnHome():
     return(render_template("learn/learnHome.html"))
 
+
 @learn.route('/practice')
 def practice():
     return(render_template("learn/practice.html"))
+
+@learn.route('/practice/_get_problems')
+def practice_get_problems():
+    problems = db.session.query(Problem).all()
+    return jsonify(problems=[{'problem': problem.question, 'answer': problem.answer,} for problem in problems])
+
+@learn.route('/practice/_get_answer_input')
+def practice_get_answer_input():
+    ans = reqiest.args.get('answer', "", type=str)
+    if current_user.is_authenticated:
+        poster = current_user.id
+    else:
+        poster = 1
+
+    return jsonify(answer="The answer is: " + solution)
