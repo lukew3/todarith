b78cb7910853e2be9961011591f57485b6a13c19
lukew3 <lukew25073@gmail.com>
2020-08-27 13:55:57 -0400

    moved some functions to main.js

--- todarith/static/js/sort.js
+++ todarith/static/js/sort.js
@@ -56,41 +56,6 @@ $(function() {
   });
 });
 
-function removeSkill(skillId, probId) {
-  $.getJSON($SCRIPT_ROOT + '/db/_remove_skill', {
-    skillId: skillId,
-    probId: probId,
-  }, function(data) {
-  });
-  skillListItem= document.getElementById("sortSkill_" + skillId.toString())
-  skillListItem.parentNode.removeChild(skillListItem);
-  console.log("Skill Removed");
-}
-
-function addSkill() {
-  console.log(selectedSkillId + " added");
-  if (curProbId != undefined ) {
-    probId = curProbId;
-  } else {
-    console.log("no skill selected")
-  }
-  $.getJSON($SCRIPT_ROOT + '/db/_add_skill', {
-    probId: probId,
-    skillId: selectedSkillId,
-  }, function(data) {
-    displaySkill(data.name, data.id);
-    console.log("yep");
-  });
-}
-
-function createSkill() {
-  $.getJSON($SCRIPT_ROOT + '/db/_create_skill', {
-    skillName: document.querySelector('#sort_addSkillQuery').value,
-  }, function(data) {
-    selectedSkillId = data.id;
-    addSkill()
-  });
-}
 
 function queryEdited() {
   const queryBox = document.querySelector('#sort_addSkillQuery');
