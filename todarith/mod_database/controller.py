10eb558b7f566b63ce141e64ca81ff4eb91a31f1
lukew3 <lukew25073@gmail.com>
2020-09-04 13:47:18 -0400

    changed uuids back to ints

--- todarith/mod_database/controller.py
+++ todarith/mod_database/controller.py
@@ -260,7 +260,7 @@ def sort_next_problem():
 @moddb.route('/_add_skill')
 def add_skill():
     skillId = request.args.get('skillId', 0, type=int)
-    probId = request.args.get('probId', "", type=str)
+    probId = request.args.get('probId', 0, type=int)
     prob = Problem.query.filter_by(id=probId).first()
     prob.sortRating = 1
     skill = Skill.query.filter_by(id=skillId).first()
@@ -281,7 +281,7 @@ def create_skill():
 def remove_skill():
     print("made it here")
     skillId = request.args.get('skillId', 0, type=int)
-    probId = request.args.get('probId', "", type=str)
+    probId = request.args.get('probId', 0, type=int)
     prob = Problem.query.filter_by(id=probId).first()
     skill = Skill.query.filter_by(id=skillId).first()
 
@@ -290,7 +290,7 @@ def remove_skill():
     return ""
 
 
-@moddb.route("/viewProb/<string:problem_id>")
+@moddb.route("/viewProb/<int:problem_id>")
 def viewProblem(problem_id):
     problem = Problem.query.filter_by(id=problem_id).first()
     return render_template('database/problem.html', problem=problem)
